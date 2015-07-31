3a22224864a12c6a1efed21f27339419a8b5827c
tnakevski <tnakevski@yahoo.com>
2015-07-31 22:19:47 +0200

    Added Infrastructure Data layer and set up db context

---
+++ eCinema/eCinema.Infrastructure.DataMSSQL/Repositories/BaseRepository.cs
@@ -0,0 +1,38 @@
+﻿using eCinema.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCinema.Infrastructure.DataMSSQL.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity>
+        where TEntity : class
+    {
+        public void Create(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ eCinema/eCinema.Infrastructure.DataMSSQL/eCinemaDbContext.cs
@@ -0,0 +1,30 @@
+﻿using eCinema.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCinema.Infrastructure.DataMSSQL
+{
+    public class eCinemaDbContext : DbContext
+    {
+        public eCinemaDbContext()
+            : base("eCinemaDbContext")
+        {
+
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Genre> Genres{ get; set; }
+        public DbSet<Hall> Halls{ get; set; }
+        public DbSet<Movie> Movies{ get; set; }
+        public DbSet<MovieActor> MovieActor { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<ShowTime> ShowTimes{ get; set; }
+        public DbSet<Ticket> Tickets{ get; set; }
+    }
+
+}
