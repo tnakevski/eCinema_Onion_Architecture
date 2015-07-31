6e1cd994d0e3ac5f5d06c5d11c3de526929c3a43
tnakevski <tnakevski@yahoo.com>
2015-08-01 00:06:50 +0200

    Added the service and test service to generate DB ( GenerateDBService  )

---
+++ eCinema/eCinema.Infrastructure.AppServices/Services/BaseService.cs
@@ -0,0 +1,18 @@
+﻿using eCinema.Infrastructure.DataMSSQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCinema.Infrastructure.AppServices.Services
+{
+    public class BaseService
+    {
+        protected UnityOfWork _uWork;
+        public BaseService(eCinemaDbContext context) 
+        {
+            _uWork = new UnityOfWork(context);
+        }
+    }
+}
---
+++ eCinema/eCinema.Infrastructure.AppServices/Services/GenerateDBService.cs
@@ -0,0 +1,25 @@
+﻿using eCinema.Core.Entities;
+using eCinema.Infrastructure.DataMSSQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCinema.Infrastructure.AppServices.Services
+{
+    public class GenerateDBService : BaseService
+    {
+
+        public GenerateDBService(eCinemaDbContext context)
+            :base(context)
+        {
+
+        }
+
+        public List<Movie> GenerateDB()
+        {
+            return _uWork.MovieRepository.GetAll();
+        }
+    }
+}
