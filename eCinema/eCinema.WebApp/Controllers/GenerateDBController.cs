43a595bfb192136eade8b717111b88629243ed98
tnakevski <tnakevski@yahoo.com>
2015-08-01 00:27:30 +0200

    Added fourth layer, the WebApp layer and made test controller to generate db ( it worked :) )

---
+++ eCinema/eCinema.WebApp/Controllers/GenerateDBController.cs
@@ -0,0 +1,32 @@
+﻿using eCinema.Infrastructure.AppServices.Services;
+using eCinema.Infrastructure.DataMSSQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCinema.WebApp.Controllers
+{
+    public class GenerateDBController : Controller
+    {
+        //
+        // GET: /GenerateDB/
+        GenerateDBService _dbService;
+        public GenerateDBController()
+        {
+            eCinemaDbContext _context = new eCinemaDbContext();
+            _dbService = new GenerateDBService(_context);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListMovies()
+        {
+            var movies = _dbService.GenerateDB();
+            return View(movies);
+        }
+	}
+}
---
+++ eCinema/eCinema.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eCinema.WebApp.MvcApplication" Language="C#" %>
