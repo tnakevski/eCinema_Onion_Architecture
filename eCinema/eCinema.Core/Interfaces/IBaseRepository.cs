4802d469b448804f4073c2e2c8dc315b2b0f711f
tnakevski <tnakevski@yahoo.com>
2015-07-31 22:03:04 +0200

    Added Base Repository Interface

---
+++ eCinema/eCinema.Core/Interfaces/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCinema.Core.Interfaces
+{
+    public interface IBaseRepository<TEntity>
+        where TEntity : class
+    {
+        void Create(TEntity entity);
+        void Delete(TEntity entity);
+        List<TEntity> GetAll();
+        TEntity GetById();
+        void Update(TEntity entity);
+    }
+}
